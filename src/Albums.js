e4eaf1a221d77c819c175b2c03186f761712431b
ken boyer <ken.boyer@outlook.com>
2020-08-20 07:44:28 -0500

    Convert Albums.js to hook

--- src/Albums.js
+++ src/Albums.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Albums() {
+  
+  // local state
+  const [albums, setAlbums] = useState([]);
 
-  const album1 = {title: "first title", artist: "first artist", numberOfTracks: 1, price: 1.1};
-  const album2 = {title: "second title", artist: "second artist", numberOfTracks: 2, price: 2.2};
-  const album3 = {title: "third title", artist: "third artist", numberOfTracks: 3, price: 3.3};
+  // effects
+  useEffect(() => {
+    const album1 = {title: "first title", artist: "first artist", numberOfTracks: 1, price: 1.112};
+    const album2 = {title: "second title", artist: "second artist", numberOfTracks: 2, price: 2.2};
+    const album3 = {title: "third title", artist: "third artist", numberOfTracks: 3, price: 3.33};
+    const responseData = [album1, album2, album3];
 
-  const albums = [album1, album2, album3];
+    setAlbums(responseData);
+  }, []);
 
+  // renderer helpers
   const renderAlbumList = (album, index) => {
     return (
       <li key={index}>
